7d9ca06b8bb3063468255e2ebcfcef5defdbd3cd
arzumanov93 <lbustamante95@mail.ru>
2021-07-01 17:30:18 +0300

    errors, authorization, navbar
    Обработка ошибок,
    авторизация,
    работа с компонентом Navbar,
    бизнес логика на сервере

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
+import { AuthContext } from '../context/AuthContext';
 
 export const AuthPage = () => {
-    const {loading, request} = useHttp();
+    const auth = useContext(AuthContext);
+    const message = useMessage();
+    const {loading, request, error, clearError} = useHttp();
     const [form, setForm] = useState({
         email: '', password: ''
     });
 
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError]);
+
+    useEffect(() => {
+        window.M.updateTextFields()
+    }, []);
+
     const changeHandler = event => {
-        setForm({ ...form, [event.target.name]: event.target.value })
+        setForm({ ...form, [event.target.name]: event.target.value });
     }
 
     const registerHandler = async () => {
-        try{
+        try {
             const data = await request('/api/auth/register', 'POST', {...form});
-            console.log('Data', data)
-        } catch (e) {
+            message(data.message);
+        } catch (e) {}
+    }
 
-        }
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form});
+            auth.login(data.token, data.userId)
+        } catch (e) {}
     }
 
     return (
@@ -58,7 +76,8 @@ export const AuthPage = () => {
                     <div className="card-action">
                         
                         <button 
-                        className="btn yellow darken-4" 
+                        className="btn yellow darken-4"
+                        onClick={loginHandler} 
                         style={{marginRight: 10}}
                         disabled={loading}
                         >Войти
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -27,10 +27,10 @@ router.post(
 
         const {email, password} = req.body
 
-        const candidate = await User.findOne({ email })
+        const candidate = await User.findOne({ email });
 
         if (candidate) {
-            return res.status(400).json({ message: 'Такой пользователь уже существует'})
+            return res.status(400).json({ message: 'Такой пользователь уже существует'});
         }
 
         const hashedPassword = await bcrypt.hash(password, 12);
@@ -54,7 +54,7 @@ router.post(
     ],
     async (req, res) => {
     try {
-        const errors = validationResult(req)
+        const errors = validationResult(req);
         
         if (!errors.isEmpty()) {
             return res.status(400).json({
@@ -71,7 +71,7 @@ router.post(
             return res.status(400).json({ message: 'Пользователь не найден' });
         }
 
-        const isMatch = await bcrypt.compare(password, user.password)
+        const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
             return res.status(400).json({ message: 'Неверный пароль, попробуйте снова' });
@@ -90,4 +90,4 @@ router.post(
     }  
 });
 
-module.exports = router
+module.exports = router;
