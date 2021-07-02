37b38b833bda175c2ed8bdd84f0fab5d1ffe8195
arzumanov93 <lbustamante95@mail.ru>
2021-07-02 15:45:40 +0300

    completion of the project
    Завершение проекта, добавление скриптов

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import 'materialize-css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import {useRoutes} from './routes';
-import {useAuth} from './hooks/auth.hook';
-import {AuthContext} from './context/AuthContext';
-import {Navbar} from './components/Navbar';
-import {Loader} from './components/Loader';
+import React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useRoutes} from './routes'
+import {useAuth} from './hooks/auth.hook'
+import {AuthContext} from './context/AuthContext'
+import {Navbar} from './components/Navbar'
+import {Loader} from './components/Loader'
+import 'materialize-css'
 
 function App() {
-  const {token, login, logout, userId, ready} = useAuth();
-  const isAuthenticated = !!token;
-  const routes = useRoutes(isAuthenticated);
+  const {token, login, logout, userId, ready} = useAuth()
+  const isAuthenticated = !!token
+  const routes = useRoutes(isAuthenticated)
 
   if (!ready) {
     return <Loader />
@@ -27,7 +27,7 @@ function App() {
         </div>
       </Router>
     </AuthContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
